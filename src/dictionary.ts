888167b5aa6c6486e1398145133761c2da6922b6
Dylan Harness <dharness@Dylans-MacBook-Air.local>
2023-11-10 08:55:26 -0500

    rename variables in dictionary file

--- src/dictionary.ts
+++ src/dictionary.ts
@@ -1,11 +1,15 @@
 import { Trie } from "./trie";
 
-// Load the dictionary from a file.
-const path = "./data/dictionary.txt";
-const file = Bun.file(path);
-const text = await file.text();
+const DICTIONARY_FILE_PATH = "./data/dictionary.txt";
 
-// Store the words as a trie.
-const dictionary = new Trie(text.split("\n"));
+async function loadDictionary(path: string): Promise<Trie> {
+  // Load the dictionary from a file.
+  const file = Bun.file(path);
+  const text = await file.text();
 
+  // Store the words as a trie.
+  return new Trie(text.split("\n"));
+}
+
+const dictionary = await loadDictionary(DICTIONARY_FILE_PATH);
 export default dictionary;
